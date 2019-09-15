87efca4e36cb715588e760ad8b9c01f3b2654c7d
Eden Swissa <edenswissa@Edens-MacBook-Pro.local>
2019-09-16 00:38:19 +0300

    forth commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  weatherApi: 'http://dataservice.accuweather.com/',
+  weatherApi: 'https://dataservice.accuweather.com/',
   apiKey:'kuf89cEP95lMaPUdWylkO1IN5La17iay'
 };
